4b90b70fb4c88e58841d0ba77a19c7deb6430bb0
kiet2024136y <154069408+kiet2024136y@users.noreply.github.com>
2025-02-21 19:36:04 +0700

    Update xu_ly.sh
    ..................

--- xu_ly.sh
+++ xu_ly.sh
@@ -1,20 +1,20 @@
 #!/bin/bash 
-echo -e "\e[33mdang cai dat......\e[0m"
+echo -e "\e[33m[*]dang cai dat......\e[0m"
 pkg install wget -y
 pkg install mono -y
 pkg update -y
 wget https://raw.githubusercontent.com/kiet2024136y/QuirkM/main/QuirkM.cs
-echo -e "\e[33mtai ca goi phu thuoc..\e[0m"
+echo -e "\e[33m[*]tai ca goi phu thuoc..\e[0m"
 
-echo -e "\e[32mda cai dat hoan tam\e[0m"
+echo -e "\e[32m[*]da cai dat hoan tam\e[0m"
 mcs QuirkM.cs
-echo -e "\e[32mda bien dich thanh cong\e[0m"
-echo -e "\e[33mdang tao file run.sh\e[0m"
+echo -e "\e[32m[*]da bien dich thanh cong\e[0m"
+echo -e "\e[33m[*]dang tao file run.sh\e[0m"
 echo -e "mono QuirkM.exe" > run.sh
-echo -e "\e[32mda tao thanh conm\e[32m"
+echo -e "\e[32m[*]da tao thanh conm\e[32m"
 chmod +x run.sh 
-echo -e "\e[32mda cap quyen thuc thi\e[0m"
-echo -e "\e[32mda khoi dong Qurikm\e[0m"
-echo -e "\e[33mexit:de thoat\e[0m"
+echo -e "\e[32m[*]da cap quyen thuc thi\e[0m"
+echo -e "\e[32m[*]da khoi dong Qurikm\e[0m"
+echo -e "\e[33m[*]exit:de thoat\e[0m"
 ./run.sh
 
